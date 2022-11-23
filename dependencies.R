0dfaf51073bc3650108aa42c2e6803133cb7ba10
lkook <lucasheinrich.kook@uzh.ch>
2022-11-23 16:36:44 +0100

    nop

--- dependencies.R
+++ dependencies.R
@@ -3,8 +3,8 @@
 packages <- c("Matrix", "boot", "data.table", "data.table", "dplyr", "ggjoy",
               "ggplot2", "ggrepel", "ggsci", "jsonlite", "keras", "knitr",
               "lubridate", "mgcv", "moments", "patchwork", "reticulate",
-              "safareg", "tensorflow", "tfprobability", "tidytext", "tidyverse",
-              "tm", "tram", "remotes")
+              "tensorflow", "tfprobability", "tidytext", "tidyverse", "tm",
+              "tram", "remotes")
 
 new_pkg <- packages[!(packages %in% installed.packages()[,"Package"])]
 if(length(new_pkg)) install.packages(new_pkg)
@@ -12,5 +12,6 @@ if(length(new_pkg)) install.packages(new_pkg)
 if (!require(tsdl))
   remotes::install_github("FinYang/tsdl")
 
+remotes::install_github("neural-structured-additive-learning/safareg")
 remotes::install_github("neural-structured-additive-learning/deepregression@a8de92a7374a88fc5ed859238ce45f56813743e8")
 remotes::install_github("neural-structured-additive-learning/deeptrafo@463145a67f467f4975907695fa0905374a0f282d")
