4151c1ebc552be2a85c2327951c0541e0537d8bf
lkook <lucasheinrich.kook@uzh.ch>
2022-11-23 16:25:42 +0100

    fix commit for install

--- dependencies.R
+++ dependencies.R
@@ -1,10 +1,10 @@
 # Install dependencies
 
-packages <- c("Matrix", "boot", "data.table", "data.table", "deepregression",
-              "deeptrafo", "dplyr", "ggjoy", "ggplot2", "ggrepel", "ggsci",
-              "jsonlite", "keras", "knitr", "lubridate", "mgcv", "moments",
-              "patchwork", "reticulate", "safareg", "tensorflow",
-              "tfprobability", "tidytext", "tidyverse", "tm", "tram", "remotes")
+packages <- c("Matrix", "boot", "data.table", "data.table", "dplyr", "ggjoy",
+              "ggplot2", "ggrepel", "ggsci", "jsonlite", "keras", "knitr",
+              "lubridate", "mgcv", "moments", "patchwork", "reticulate",
+              "safareg", "tensorflow", "tfprobability", "tidytext", "tidyverse",
+              "tm", "tram", "remotes")
 
 new_pkg <- packages[!(packages %in% installed.packages()[,"Package"])]
 if(length(new_pkg)) install.packages(new_pkg)
@@ -12,3 +12,5 @@ if(length(new_pkg)) install.packages(new_pkg)
 if (!require(tsdl))
   remotes::install_github("FinYang/tsdl")
 
+remotes::install_github("neural-structured-additive-learning/deepregression@a8de92a7374a88fc5ed859238ce45f56813743e8")
+remotes::install_github("neural-structured-additive-learning/deeptrafo@463145a67f467f4975907695fa0905374a0f282d")
