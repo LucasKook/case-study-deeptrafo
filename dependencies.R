70608741b704a57f53310f790fb1506fed6829fd
lkook <lucasheinrich.kook@uzh.ch>
2022-11-23 15:22:58 +0100

    tsdl install

--- dependencies.R
+++ dependencies.R
@@ -4,8 +4,11 @@ packages <- c("Matrix", "boot", "data.table", "data.table", "deepregression",
               "deeptrafo", "dplyr", "ggjoy", "ggplot2", "ggrepel", "ggsci",
               "jsonlite", "keras", "knitr", "lubridate", "mgcv", "moments",
               "patchwork", "reticulate", "safareg", "tensorflow",
-              "tfprobability", "tidytext", "tidyverse", "tm", "tram", "tsdl")
+              "tfprobability", "tidytext", "tidyverse", "tm", "tram", "remotes")
 
 new_pkg <- packages[!(packages %in% installed.packages()[,"Package"])]
 if(length(new_pkg)) install.packages(new_pkg)
+
+if (!require(tsdl))
+  remotes::install_github("FinYang/tsdl")
 
